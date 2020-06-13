9d929cf16ba29e425cffc8dd62c49edf8f9fb1ad
Joy Dutta <joy@securiti.ai>
2020-06-12 18:59:30 -0600

    Initial engagement bars with real data.

--- js/viz.js
+++ js/viz.js
@@ -9,36 +9,60 @@
             right: 10,
             bottom: 10,
             left: 10
-        }
+        },
+        barHeight: 20,
+        barGap: 5
     }
+    // let engagementDataFile = 'data/distracted_engaged_info.json'
+    let engagementDataFile = 'data/prez1_all_users_gotcha_times.json'
+    // let quizDataFile = 'data/zombie_responses_info.json'
+    let quizDataFile = 'data/prez1_ruts_data.json'
 
     function init() {
-        console.log('init', distractedEngagedData, quizData)
+        // console.log('init', distractedEngagedData, quizData)
         vizWrapperEl = document.getElementById('vizWrapper')
         vizEl = document.getElementById('viz')
-        drawChart()
+        drawChart({
+            engagementData: distractedEngagedData,
+            quizData: quizData
+        })
     }
 
     function loadDataFiles() {
-        let p1 = d3.json('/data/distracted_engaged_info.json').then(data => {
+        let p1 = d3.json(engagementDataFile).then(data => {
             distractedEngagedData = data
-            // distractedEngagedData = _.filter(distractedEngagedData, o => o.event === 'attentive')
-            // distractedEngagedData = _.groupBy(distractedEngagedData, o => o.user_id)
         })
-        let p2 = d3.json('/data/zombie_responses_info.json').then(data => {
+        let p2 = d3.json(quizDataFile).then(data => {
             quizData = data
         })
         return Promise.all([p1, p2])
     }
 
-    function drawChart(containerSize) {
-        let data = {}
+    function processData(payload) {
+        let filteredEngagementData = _.filter(payload.engagementData, o => o.event === 'attentive')
+        let userToEngagementData = _.groupBy(filteredEngagementData, o => o.user_id)
+        console.log('userToEngagementData', userToEngagementData)
+        let minTs = _.min(filteredEngagementData.map(o => new Date(o.start_time).getTime()))
+        let maxTs = _.max(filteredEngagementData.map(o => new Date(o.end_time).getTime()))
+        console.log('minTs',minTs, maxTs)
 
-        containerSize = containerSize || {
-            w: vizWrapperEl.offsetWidth,
-            h: vizWrapperEl.offsetHeight
+        return {
+            userToEngagementData,
+            users: Object.keys(userToEngagementData),
+            minTs,
+            maxTs
         }
+    }
+
+    function drawChart(payload) {
+        let data = processData(payload)
+
         let margin = config.margin
+        let containerSize = {
+            // w: vizWrapperEl.offsetWidth,
+            w: 3000,
+            h: Math.max(vizWrapperEl.offsetHeight, margin.top + margin.bottom + data.users.length * (config.barHeight + config.barGap))
+        }
 
         let width = containerSize.w - margin.left - margin.right
         let height = containerSize.h - margin.top - margin.bottom
@@ -49,6 +73,45 @@
             .attr('width', containerSize.w)
             .attr('height', containerSize.h)
         let gRoot = svg.append('svg:g').attr('transform', 'translate(' + gRootXY + ')')
+
+        let xScale = d3.scaleLinear()
+            .domain([data.minTs, data.maxTs])
+            .range([0, width])
+
+        data.users.forEach((userId, userIndex) => {
+            let engagementData = data.userToEngagementData[userId]
+            let gUserEngagement = gRoot.append('svg:g')
+                        .classed('user-engagement', true)
+                .attr('transform', 'translate('+[0, userIndex * (config.barHeight + config.barGap)]+')')
+            gUserEngagement.append('svg:rect')
+                .classed('distraction', true)
+                .attr('x', 0)
+                .attr('y', 0)
+                .attr('width', width)
+                .attr('height', config.barHeight)
+            gUserEngagement.append('svg:text')
+                .attr('x', 0)
+                .attr('y', 15)
+                .text(`User ${userId}`)
+            engagementData.forEach(o => {
+                let startTs = new Date(o.start_time).getTime()
+                let endTs = new Date(o.end_time).getTime()
+                let x1 = xScale(startTs)
+                let x2 = xScale(endTs)
+                if (x2 - x1 < 0) {
+                    return
+                }
+                // console.log('x1 x2', x1, x2)
+                gUserEngagement.append('svg:rect')
+                    .classed('attention', true)
+                    .attr('x', x1)
+                    .attr('y', 0)
+                    .attr('width', x2 - x1)
+                    .attr('height', config.barHeight)
+                    .append('title')
+                    .text(`Attention span for user ${o.user_id} from ${o.start_time} to ${o.end_time}`)
+            })
+        })
     }
 
     loadDataFiles().then(init)
